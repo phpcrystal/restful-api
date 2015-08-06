7f7a3da38ad43111d275206a7746171bfd600be1
Andrei Samuilik <a.samuylik@gmail.com>
2015-08-06 12:14:47 +0300

    Composer package name

--- bootstrap.php
+++ bootstrap.php
@@ -3,5 +3,6 @@ namespace PHPCrystal\RESTful\Facade;
 
 return Extension::create()
 	->setDirectory(__DIR__)
+	->setComposerName('phpcrystal/restful-api')
 	->setRouter('PHPCrystal\\RESTful\\Service\\Router\\Restful')
 ;
